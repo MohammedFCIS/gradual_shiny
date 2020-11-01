9a591d5b5e76eefa9a6fb0f19df10bc93269a002
Mohammed Ali <mali@uplandsoftware.com>
2020-11-01 08:11:29 +0200

    add Reactives

--- 02_reactives/app.R
+++ 02_reactives/app.R
@@ -12,57 +12,58 @@ library(fivethirtyeight)
 data(biopics)
 biopics <- biopics %>% filter(!is.na(box_office))
 ##specify what categories we want to color with
-select_color_options <- c("type_of_subject", "subject_race", "subject_sex")
+select_color_options <-
+   c("type_of_subject", "subject_race", "subject_sex")
 
 min_year <- min(biopics$year_release)
 max_year <- max(biopics$year_release)
 
 
-# Define UI for application that plots 
-ui <- fluidPage(
-   
-   # Application title
+# Define UI for application that plots
+ui <- fluidPage(# Application title
    titlePanel("Adding a Reactive"),
    
-   # Sidebar with a slider input for number of bins 
+   # Sidebar with a slider input for number of bins
    sidebarLayout(
       sidebarPanel(
-        selectInput("color_opts", "Select Category to Color With",
-                    choices = select_color_options),
-        sliderInput("year_filter", "Select Lowest Year", min = min_year,
-                    max=max_year, value = min_year)
+         selectInput("color_opts", "Select Category to Color With",
+                     choices = select_color_options),
+         sliderInput(
+            "year_filter",
+            "Select Lowest Year",
+            min = min_year,
+            max = max_year,
+            value = min_year
+         )
       ),
       
       # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("scatter_plot"),
-         plotOutput("boxoffice_boxplot")
-      )
-   )
-)
+      mainPanel(plotOutput("scatter_plot"),
+                plotOutput("boxoffice_boxplot"))
+   ))
 
 ##Server is where all of the computations happen
 server <- function(input, output) {
-  
-  biopics_filtered <- reactive({
-    ##add code after biopics here
-    biopics 
-  })
+   biopics_filtered <- reactive({
+      ##add code after biopics here
+      biopics %>% filter(year_release > input$year_filter)
+   })
    
    output$scatter_plot <- renderPlot({
-      biopics_filtered() %>% ggplot(aes_string(y="box_office", 
-                                               x="year_release", 
-                                               color=input$color_opts)) + 
-       geom_point()
-     
+      biopics_filtered() %>% ggplot(aes_string(
+         y = "box_office",
+         x = "year_release",
+         color = input$color_opts
+      )) +
+         geom_point()
+      
    })
    
    output$boxoffice_boxplot <- renderPlot({
-     biopics %>% ggplot(aes_string(x=input$color_opts, y="box_office")) + 
-       geom_boxplot() + theme(axis.text.x = element_text(angle=45))
+      biopics %>% ggplot(aes_string(x = input$color_opts, y = "box_office")) +
+         geom_boxplot() + theme(axis.text.x = element_text(angle = 45))
    })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
-
