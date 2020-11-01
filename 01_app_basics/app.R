c9724b5afd9dcf023c8d142019b53154a21de552
Mohammed Ali <mali@uplandsoftware.com>
2020-11-01 07:51:40 +0200

    Making a ggplot interactive

--- 01_app_basics/app.R
+++ 01_app_basics/app.R
@@ -12,49 +12,49 @@ library(fivethirtyeight)
 data(biopics)
 biopics <- biopics %>% filter(!is.na(box_office))
 ##specify what categories we want to color with
-select_color_options <- c("type_of_subject", "subject_race", "subject_sex")
+select_color_options <-
+   c("type_of_subject", "subject_race", "subject_sex")
 min_year <- min(biopics$year_release)
 max_year <- max(biopics$year_release)
 
 
-# Define UI for application that plots 
+# Define UI for application that plots
 ui <- fluidPage(
-   
    # Application title
    titlePanel("Part 1: Connecting UI and Server"),
    
-   # Sidebar with a slider input for number of bins 
+   # Sidebar with a slider input for number of bins
    sidebarLayout(
       sidebarPanel(
-        ## Add User Interface element here
-        
+         selectInput("color_opts", "Select Category to Color With",
+                     choices = select_color_options)
+         
       ),
       
       # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("scatter_plot"),
-        plotOutput("boxoffice_boxplot")
-      )
+      mainPanel(plotOutput("scatter_plot"),
+                plotOutput("boxoffice_boxplot"))
    )
 )
 
 ##Server is where all of the computations happen
 server <- function(input, output) {
-  
    output$scatter_plot <- renderPlot({
-      biopics %>% ggplot(aes_string(y="box_office", x="year_release", 
-                                               color="type_of_subject")) + 
-       geom_point()
-     
+      biopics %>% ggplot(aes_string(
+         y = "box_office",
+         x = "year_release",
+         color = input$color_opts
+      )) +
+         geom_point()
+      
    })
    
    output$boxoffice_boxplot <- renderPlot({
-     biopics %>% ggplot(aes_string(x="type_of_subject", y="box_office", 
-                                   fill="type_of_subject")) + 
-       geom_boxplot() + theme(axis.text.x = element_text(angle=45))
+      biopics %>% ggplot(aes_string(x = "type_of_subject", y = "box_office",
+                                    fill = "type_of_subject")) +
+         geom_boxplot() + theme(axis.text.x = element_text(angle = 45))
    })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
-
